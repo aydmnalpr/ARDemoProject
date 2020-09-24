e1619208faa3d9c74787d545a3ddf6bed16319c3
Alper Ayduman <aydumanalper@gmail.com>
2020-09-24 13:59:34 +0300

    Initial commit

---
+++ Assets/ButtonPopup.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+using static Vuforia.TrackableBehaviour;
+
+
+public class ButtonPopup : MonoBehaviour {
+	
+    private TrackableBehaviour mTrackableBehaviour;
+	
+    private bool mShowGUIButton = false;
+    private Rect mButtonRect = new Rect(50,50,120,60);
+	
+    void Start () {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterOnTrackableStatusChanged(OnTrackableStateChanged);
+        }
+    }
+	
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.StatusChangeResult obj)
+    {
+        if (obj.NewStatus == Status.DETECTED ||
+            obj.NewStatus == Status.TRACKED)
+        {
+            mShowGUIButton = true;
+        }
+        else
+        {
+            mShowGUIButton = false;
+        }
+    }
+	
+    void OnGUI() {
+        if (mShowGUIButton) {
+            // draw the GUI button
+            if (GUI.Button(mButtonRect, "Hello")) {
+                // do something on button click	
+            }
+        }
+    }
+}
